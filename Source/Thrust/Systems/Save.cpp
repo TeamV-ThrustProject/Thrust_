9db89478673b7051f59525d6ed96a22ab8429b9b
taesoo-jeong <jp430612@naver.com>
2024-10-08 21:18:59 +0900

    근접적에 모델 추가 및 그에 맞춰 ABP 및 애니메이션 적용

--- Source/Thrust/Systems/Save.cpp
+++ Source/Thrust/Systems/Save.cpp
@@ -11,26 +11,26 @@ ASave::ASave()
 
 	CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("CollisionBox"));
 	CollisionBox->SetupAttachment(RootComponent);
-	CollisionBox->SetCollisionProfileName("Trigger");
+	CollisionBox->SetCollisionProfileName("Trigger");	//충돌을 트리거로 설정
+	CollisionBox->SetGenerateOverlapEvents(true);	//오버랩 이벤트 활성화
+	CollisionBox->SetCollisionProfileName(TEXT("OverlapAllDynamic"));	//**충돌 채널 설정**
 
 	CollisionBox->OnComponentBeginOverlap.AddDynamic(this, &ASave::OnOverlapBegin);
-
 }
 
 void ASave::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
-void ASave::OnOverlapBegin(UPrimitiveComponent* OverlappedComp,
-	AActor* OtherActor, UPrimitiveComponent* OtherComp,
-	int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+void ASave::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, 
+	AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+	bool bFromSweep, const FHitResult& SweepResult)
 {
 	AThrustCharacter* Player = Cast<AThrustCharacter>(OtherActor);
 	if (Player)
 	{
-		//Player->LastSavedTransform = this->GetActorTransform();
+		Player->LastSavedTransform.SetLocation(this->GetActorTransform().GetLocation());
 	}
 }
 
